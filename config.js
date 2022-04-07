a5815550e4bf7ee6b12d0588b178a5e834123501
Shakhan <khan.vinn@gmail.com>
2022-04-07 20:33:47 +0100

    alpha methods of main component is ready

---
+++ config.js
@@ -0,0 +1,3 @@
+export default {
+    "UNSPLASH_ACCESS_KEY":"Oe5DWpCAi3XhGKpc3sgrbWJ2hfxh8VN3dly8B9uyDmM"
+}
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="content">
+    <div class="photos">
+      <div v-for="photo  in this.randomPhotos" :key="photo">
+       <div> {{photo.user.id}} </div>
+        <img :src=photo.urls.small>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {createApi} from "unsplash-js";
+import config from "../config";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      randomPhotos: [],
+      favourite_photos: [],
+      searchInput: ""
+    }
+  },
+  mounted() {
+    this.unsplash = createApi({
+      accessKey: config.UNSPLASH_ACCESS_KEY
+    })
+    this.unsplash.photos.getRandom({count:8}).then(data=> {
+      this.randomPhotos = data.response
+    }).catch(e=> {
+      console.log(e)
+    })
+  },
+  methods:{
+    addToFavourites(item){
+      try{
+        this.favourite_photos.push(item)
+        return true
+      }catch (e) {
+        console.log(e)
+        return false
+      }
+    },
+    searchPhotos(){
+      this.unsplash.search.getPhotos({
+        query:this.searchInput || "",
+        perPage: 8
+      }).then(data=> {
+        this.randomPhotos = data.response
+      }).catch(e=> {
+        console.log(e)
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
