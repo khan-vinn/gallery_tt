e6e5faf01d2d685676663db330cc305735f390b5
Shakhan <khan.vinn@gmail.com>
2022-04-08 11:09:57 +0100

    all functions will anounced, and will do CSS

---
+++ src/components/Photo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img :src="photo.urls.small" :alt="photo.user.id" @click="addToFavourite(photo)"/>
+    <div>
+      <p> {{ photo.user.name }}</p>
+      <img :src="photo.user.profile_image.small" :alt="photo.user.portfolio_url" >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PhotoComponent",
+  props: {
+    photo: Object,
+    addToFavourite: Function
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Photos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="photos" v-if="photos?.length>0">
+    <div v-for="photo  in photos" :key="photo">
+      <div @click="addToFavourite(photo)"> {{ photo.user.id }}</div>
+      <img :src=photo.urls.small :alt=photo.user.name @click="select(photo)">
+    </div>
+  </div>
+  <div v-else>
+    No photos
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PhotosComponent",
+  props:{
+    photos: Array,
+    addToFavourite: Function,
+    select: Function
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
